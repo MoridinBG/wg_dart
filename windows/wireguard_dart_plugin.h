096103404599b79660a710fa409163aa14a88828
Ivan Dilchovski <ivan.dilchovski@tumba.solutions>
2025-07-02 00:12:46 +0300

    Create an enum of supported methods. Move method calls to their own methods and match over the enum of supported methods

--- windows/wireguard_dart_plugin.h
+++ windows/wireguard_dart_plugin.h
@@ -5,28 +5,56 @@
 #include <flutter/plugin_registrar_windows.h>
 
 #include <memory>
+#include <optional>
 
-#include "service_control.h"
 #include "connection_status_observer.h"
+#include "service_control.h"
 
 namespace wireguard_dart {
 
+enum class WireguardMethod {
+  GENERATE_KEY_PAIR,
+  CHECK_TUNNEL_CONFIGURATION,
+  NATIVE_INIT,
+  SETUP_TUNNEL,
+  CONNECT,
+  DISCONNECT,
+  STATUS
+};
+
 class WireguardDartPlugin : public flutter::Plugin {
  public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows* registrar);
 
   WireguardDartPlugin();
 
   virtual ~WireguardDartPlugin();
 
   // Disallow copy and assign.
-  WireguardDartPlugin(const WireguardDartPlugin &) = delete;
-  WireguardDartPlugin &operator=(const WireguardDartPlugin &) = delete;
+  WireguardDartPlugin(const WireguardDartPlugin&) = delete;
+  WireguardDartPlugin& operator=(const WireguardDartPlugin&) = delete;
 
  private:
   // Called when a method is called on this plugin's channel from Dart.
-  void HandleMethodCall(const flutter::MethodCall<flutter::EncodableValue> &method_call,
+  void HandleMethodCall(const flutter::MethodCall<flutter::EncodableValue>& method_call,
                         std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+
+  // Helper methods for each supported method
+  std::optional<WireguardMethod> GetMethodFromString(const std::string& method_name);
+  void HandleGenerateKeyPair(const flutter::EncodableMap* args,
+                             std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+  void HandleCheckTunnelConfiguration(const flutter::EncodableMap* args,
+                                      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+  void HandleNativeInit(const flutter::EncodableMap* args,
+                        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+  void HandleSetupTunnel(const flutter::EncodableMap* args,
+                         std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+  void HandleConnect(const flutter::EncodableMap* args,
+                     std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+  void HandleDisconnect(const flutter::EncodableMap* args,
+                        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+  void HandleStatus(const flutter::EncodableMap* args,
+                    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
 
   std::unique_ptr<ServiceControl> tunnel_service_;
   std::unique_ptr<ConnectionStatusObserver> connection_status_observer_;
